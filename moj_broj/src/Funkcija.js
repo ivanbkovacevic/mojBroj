d5150a58b5cbb01c2179c1c9c0132482d87e5687
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-09-12 17:49:04 +0200

    funkcija

--- moj_broj/src/Funkcija.js
+++ moj_broj/src/Funkcija.js
@@ -9,28 +9,8 @@ class Funkcija extends Component {
         target:196
     }
 
-    f = (a, b, opr) => {
-        if ('+' === opr) { return a + b; }
-        if ('*' === opr) { return a * b; }
-        if ('-' === opr) { return a - b; }
-        if ('/' === opr) { return a / b; }
-    }
-
+ 
     poredjenje = () => {
-        let { opr, a, rez } = this.state;
-        let r = null;
-        for (let i = 1; i < a.length; i++) {
-            for (let j = 0; j < opr.length; j++) {
-
-                let r = this.f(this.f(a[i - 1], a[i], opr[j]), a[i], opr[j]);
-
-                rez.push(r);
-            }
-        }
-        this.setState({ rez });
-    }
-
-    poredjenje2 = () => {
         let { opr, a, rez,target } = this.state;
         for (let n = 0; n < a.length; n++) {
             for (let m = 0; m < a.length; m++) {
@@ -50,23 +30,23 @@ class Funkcija extends Component {
                                         rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k])
                                         console.log(rez+'rez-rr')
                                     
-                                    // let rrr = this.ff(rr, a[l], opr[o]);
-                                    // if(rrr ===target){
-                                    //     rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k]+opr[o]+a[l])
-                                    //     console.log(rez)
-                                    // }
+                                    let rrr = this.ff(rr, a[l], opr[o]);
+                                    if(rrr ===target){
+                                        rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k]+opr[o]+a[l])
+                                        console.log(rez)
+                                    }
                                  
-                                    // let rrrr = this.ff(rrr, a[m], opr[o]);
-                                    // if(rrrr ===target){
-                                    //     rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k]+opr[o]+a[l]+opr[o]+a[m])
-                                    //     console.log(rez)
-                                    // }
+                                    let rrrr = this.ff(rrr, a[m], opr[o]);
+                                    if(rrrr ===target){
+                                        rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k]+opr[o]+a[l]+opr[o]+a[m])
+                                        console.log(rez)
+                                    }
                                   
-                                    // let rrrrr = this.ff(rrrr, a[n], opr[o]);
-                                    // if(rrrrr ===target){
-                                    //     rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k]+opr[o]+a[l]+opr[o]+a[m]+opr[o]+a[n])
-                                    //     console.log(rez)
-                                    // }
+                                    let rrrrr = this.ff(rrrr, a[n], opr[o]);
+                                    if(rrrrr ===target){
+                                        rez= String(a[i]+opr[o]+a[j]+opr[o]+a[k]+opr[o]+a[l]+opr[o]+a[m]+opr[o]+a[n])
+                                        console.log(rez)
+                                    }
                                           
                                 }
                             }
@@ -86,6 +66,9 @@ class Funkcija extends Component {
         if ('/' === opr) { return a / b; }
     }
 
+
+
+    
     render() {
         return (
             <div>
